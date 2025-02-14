682497eb8bde8ce04199a6c30db5208b1e8008f1
Leonie Scheidweiler <leonie.olivia.scheidweiler@gmail.com>
2025-02-14 14:42:16 +0100

    added job array functionality for slurm

--- functions_run/run_remotec.py
+++ functions_run/run_remotec.py
@@ -15,6 +15,8 @@ def main(rundir, remotec_path, lst_file_list, scheduler):
             func = condor
         case "slurm":
             func = slurm
+        case "slurm_jobarrays":
+            func = slurm_jobarrays
         case _:
             sys.exit(f"scheduler {scheduler} invalid.")
 
@@ -84,3 +86,41 @@ def slurm(rundir, remotec_path, lst_file_list):
         processes.append(process)
 
     exitcodes = [process.wait() for process in processes]
+
+def slurm_jobarrays(rundir, remotec_path, lst_file_list):
+    print("using scheduler slurm with job arrays")
+    os.system(f"mkdir {rundir}/slurm")
+
+    job_file = f"{rundir}/slurm/job_array.sh"
+    outdir = f"{rundir}/slurm"
+    num_jobs = len(lst_file_list)
+
+    with open(job_file, "w") as file:
+        file.writelines("#!/bin/bash\n")
+        file.writelines("#SBATCH --partition=cpu-single\n")
+        file.writelines("#SBATCH --ntasks=1\n")
+        file.writelines("#SBATCH --time=18:00:00\n")
+        file.writelines("#SBATCH --mem=16gb\n")
+        file.writelines(f"#SBATCH --array=1-{num_jobs}\n")
+        file.writelines(f"#SBATCH --output={outdir}/out_%a.out\n")
+        file.writelines(f"#SBATCH --error={outdir}/out_%a.out\n")
+        file.writelines("printf -v padded_id '%06d' $SLURM_ARRAY_TASK_ID\n")
+        file.writelines(f"mv {outdir}/out_$SLURM_ARRAY_TASK_ID.out {outdir}/out_${{padded_id}}.out\n")
+
+        file.writelines("lst_file_list=(" + " ".join(lst_file_list) + ")\n")
+        file.writelines("lst_file=${lst_file_list[$SLURM_ARRAY_TASK_ID-1]}\n")
+        file.writelines(f"{remotec_path} $SLURM_ARRAY_TASK_ID $lst_file\n")
+
+    # execute and wait
+    # original job just starts the job array and kills itself right after
+    # this means that, to wait for all jobs to finish, we need to get the job_id to
+    # track all the smaller jobs.
+    # after all the smaller jobs are done, a dummy job that does nothing is run. the python
+    # script waits for this dummy job
+    result = subprocess.run(["sbatch", job_file], stdout=subprocess.PIPE, text=True)
+    job_id = next((word for word in result.stdout.split() if word.isdigit()), None)
+
+    if job_id is None:
+        raise RuntimeError("Failed to get SLURM job ID.")
+
+    os.system(f"sbatch --dependency=afterok:{job_id} --output={outdir}/slurm-%j.out --wrap='echo Job array {job_id} completed.' --wait")
